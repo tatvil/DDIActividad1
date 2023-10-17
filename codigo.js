80a58c0f1b126dbc9eafcfd1e5457c8f82e03b8c
Tatiana Villa <tatiana@tecnologia-facil.es>
2023-10-17 23:18:57 +0200

    First commit

---
+++ codigo.js
@@ -0,0 +1,41 @@
+let nombre;
+let etiquetaJugador;
+let articulos;
+let barraLateral;
+
+
+window.addEventListener("load",() => {
+	nombre = document.getElementById('nombre');
+    let etiquetaJugador = document.getElementById("persona");
+
+    etiquetaJugador.innerHTML = "<h3>Pepe</h3>";
+
+	
+/*	primerArticulo = parrafos[0];
+
+	//ACCESO A ELEMENTOS POR LA CLASE
+	articulos = document.getElementsByClassName('articulo');
+
+	//ACCESO A ELEMENTOS POR SU IDENTIFICADOR (ID es único)
+	barraLateral = document.getElementById('lateral');
+
+
+	//MODIFICAR ELEMENTOS DEL DOM
+	let cabecera = document.getElementById("titulo");
+	//cabecera.textContent = "Modificando el DOM";
+	cabecera.innerHTML = "<h1>Modificando el DOM</h1>";
+
+	barraLateral.classList.remove("sidebar");
+	barraLateral.classList.add("sidebar2");
+
+	barraLateral.style.backgroundColor = "#B54135";
+	barraLateral.style.color = "#FFF";
+
+	primerArticulo.addEventListener("mouseover",()=>{
+		primerArticulo.style.color = "yellow";
+	});
+	primerArticulo.addEventListener("mouseout",()=>{
+		primerArticulo.style.color = "black";
+	});
+*/
+});
