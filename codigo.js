f28f29530ea0f79cef43f03c31a5171fc523b485
Tatiana Villa <tatiana@tecnologia-facil.es>
2023-10-24 13:36:41 +0200

    añado imagenes, coloco en columnas con css

--- codigo.js
+++ codigo.js
@@ -8,16 +8,20 @@ let piedra;
 let papel;
 let tijera;
 let lagarto;
-let spok;
+let spock;
 let resultado;
+let imagenCentral;
 let ordenadorOpcion;
+let eleccionjugador;
+let eleccionordenador;
 //let ordenadorOpcion[]=("piedra","papel","tijera","lagarto","spok");
 
-/*
+
 function quienGana(jugador)
 	{
 		ordenadorOpcion="piedra";
-		resultado.textContent = "Entro aqui";
+		eleccionjugador.textContent = jugador;
+		eleccionordenador.textContent = ordenadorOpcion;
 		if (jugador == ordenadorOpcion)
 			{
 				resultado.textContent = "Empate";
@@ -27,7 +31,7 @@ function quienGana(jugador)
 				resultado.textContent = "No Empate";
 			}
 	}
-*/
+
 
 function comenzarPartida()
 	{
@@ -35,19 +39,32 @@ function comenzarPartida()
 		puntosPersona.textContent.value = puntosPersona.textContent.value + 1;
 	}
 
+function ocultarTodo()
+	{
+		persona.style.visibility="hidden";
+		ordenador.style.visibility="hidden";
+	//	document.getElementById("imagencentro").style.visibility = "hidden";
+	//    imagenCentral.style.visibility="hidden";
+	}
+
+function visualizarTodo()
+	{
+		persona.style.visibility="visible";
+		ordenador.style.visibility="visible";
+//			imagenCentral.style.visibility="visible";
+	}
+
 function verificarNombre()
 	{	
 		if(nombre.value.length == 0)
 			{
 			errorNombre.textContent ="El nombre no puede estar vacio";
-			etiquetaJugador.textContent = "Jugador";
-			persona.style.display="none";
-			ordenador.style.display="none";
+//			etiquetaJugador.textContent = "Jugador";
+			ocultarTodo();
 			}
 		else{
 			errorNombre.textContent ="Todo bien";
-			persona.style.display="block";
-			persona.style.display="block";
+			visualizarTodo();
 			etiquetaJugador.textContent = "Tu: "+nombre.value;
 
 			}
@@ -59,32 +76,35 @@ function inicializarVariables ()
     etiquetaJugador = document.getElementById("etiquetaJugador");
 	botonNombre 	= document.getElementById("botonNombre");
 	errorNombre 	= document.getElementById("errorNombre");
-	errorArticulo 	= document.getElementById("errorArticulo");
 	persona 		= document.getElementById("persona");
 	ordenador 		= document.getElementById("ordenador");
+	imagenCentral	= document.getElementById("imagencentro");
 	puntosOrdenador = document.getElementById("puntos-ordenador");
 	puntosPersona 	= document.getElementById("puntos-persona");
 	piedra    	  	= document.getElementById("piedra");
 	papel     		= document.getElementById("papel");
 	tijera    		= document.getElementById("tijera");
 	lagarto   		= document.getElementById("lagarto");
-	spok	  		= document.getElementById("spok");
+	spock	  		= document.getElementById("spock");
+	eleccionJugador = document.getElementById("eleccionjugador");
+	eleccionOrdenador = document.getElementById("eleccionordenador");
 	resultado 		= document.getElementById("resultado");
   }
 
 function setListeners()
   {
-//	nombre.addEventListener("input",verificarNombre);
+	nombre.addEventListener("onKeoy",verificarNombre);
 	botonNombre.addEventListener('click',verificarNombre);
-//	piedra.addEventListener('click',quienGana("piedra"));
-//	papel.addEventListener('click',quienGana("papel"));
-//	tijera.addEventListener('click',quienGana("tijera"));
-//	lagarto.addEventListener('click',quienGana("lagarto"));
-//	spok.addEventListener('click',quienGana("spok"));
+	piedra.addEventListener('click',quienGana("piedra"));
+	papel.addEventListener('click',quienGana("papel"));
+	tijera.addEventListener('click',quienGana("tijera"));
+	lagarto.addEventListener('click',quienGana("lagarto"));
+	spok.addEventListener('click',quienGana("spok"));
   }
 
 
 window.addEventListener("load",() => {
+		ocultarTodo();
 		inicializarVariables();
 		setListeners();
 	});
